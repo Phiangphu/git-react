df7515f0f24240471e596be2926825832a601eec
go <go.nobpadon25@gmail.com>
2021-05-28 09:50:11 +0700

    add Button

--- src/app/modules/ChangName/changName.js
+++ src/app/modules/ChangName/changName.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import FormikTextField from '../Common/components/CustomFormik/FormikTextField'
+import AddButton from '../Common/components/Buttons/AddButton'
+import { Grid } from '@material-ui/core'
 function changName() {
 	return (
-		<div>
+		<Grid
+			container
+			direction="row"
+			justify="center"
+			alignItems="center"
+		>
 			<FormikTextField></FormikTextField>
-		</div>
+			<AddButton></AddButton>
+		</Grid>
+
 	)
 }
 
