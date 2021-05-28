5eabb4cef900d8504165d19d0b5bfd1230e7a826
75023940+InsidiousChampz@users.noreply.github.com <75023940+InsidiousChampz@users.noreply.github.com>
2021-05-28 16:12:24 +0700

    1

--- src/app/modules/NewProduct/NewProduct.js
+++ src/app/modules/NewProduct/NewProduct.js
@@ -3,20 +3,7 @@ import React from 'react'
 function NewProduct() {
       return (
             <div>
-                  Producu
-                  Plot
-                  หกดหก
-                  หกดหกหกด
-                  หกดหก
-                  กเ
-                  หกเ
-                  หกดเ
-                  หกเหก
-                  หกดหกหกดหกด
-                  หกด
-                  Go
-                  123456
-                  dear6666
+                  1
             </div>
       )
 }
