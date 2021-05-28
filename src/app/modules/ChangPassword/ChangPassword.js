804ab4224e65c21d96775915bcdb42116172b083
go <go.nobpadon25@gmail.com>
2021-05-28 11:13:18 +0700

    test conflic

--- src/app/modules/ChangPassword/ChangPassword.js
+++ src/app/modules/ChangPassword/ChangPassword.js
@@ -14,13 +14,13 @@ function ChangPassword() {
 		>
 			<FormikTextField></FormikTextField>
 			<Grid item xs={12} lg={3}>
-          <FormikTextField
-            formik={formik}
-            name="test"
-            label="Test"
-            required
-          />
-        </Grid>
+				<FormikTextField
+					formik={formik}
+					name="test7841254"
+					label="Test"
+					required
+				/>
+			</Grid>
 			<FormikCheckBox></FormikCheckBox>
 			<ViewButton>View</ViewButton>
 		</Grid>
