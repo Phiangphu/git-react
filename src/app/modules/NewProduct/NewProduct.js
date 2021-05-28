ef003ad0ee521842cb04f5553a7e565034b006f5
DESKTOP-S99BLQ4\admin <immortal-1@hotmail.com>
2021-05-28 14:57:51 +0700

    add new Product

---
+++ src/app/modules/NewProduct/NewProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewProduct() {
+      return (
+            <div>
+                  Diadora Na Ja
+            </div>
+      )
+}
+
+export default NewProduct
