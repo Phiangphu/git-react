2469dbd2e009b34a3a0c5c4a741d6d5f07ed46ba
ISC-NB-COM-06\ISC-NB-COM-06 <giggryjung@gmail.com>
2021-06-04 11:19:42 +0700

    add EditUser

---
+++ src/app/modules/EditUser/EditUser.js
@@ -0,0 +1,12 @@
+import React from 'react'  
+function EditUser()
+ {     
+    return (        
+         <div>           
+             test        
+        </div>   
+        
+        )
+     }  
+     
+     export default EditUser
