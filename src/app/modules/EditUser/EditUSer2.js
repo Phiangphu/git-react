47ef73f75392c7f1689d13649b250ae14101b3d4
go <go.nobpadon25@gmail.com>
2021-06-04 11:37:59 +0700

    test confic

--- src/app/modules/EditUser/EditUSer2.js
+++ src/app/modules/EditUser/EditUSer2.js
@@ -1,12 +1,13 @@
-import React from 'react'  
-function EditUser()
- {     
-    return (        
-         <div>           
-             test 12345     
-        </div>   
-        
-        )
-     }  
-     
-     export default EditUser
+import React from 'react'
+function EditUser() {
+   return (
+      <div>
+         test 12345
+         test conflic111
+         test11
+      </div>
+
+   )
+}
+
+export default EditUser
